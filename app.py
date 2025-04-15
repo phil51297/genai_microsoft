e163e1fe3ef6c8f2f7eb7a92096d342ce087af4f
phil51297 <b.philippe.97@gmail.com>
2025-04-15 10:38:28 +0200

    init commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MedAssist - Assistant IA médical",
+    page_icon="🏥",
+    layout="wide"
+)
+
+st.title("MedAssist - Assistant IA médical")
+st.subheader("Prototype pour le Hackathon OpenCertif")
+
